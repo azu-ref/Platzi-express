3ec20df916be25e38474e773a9da11a338313c05
Fernando Azuaje <fjah17@gmail.com>
2019-04-04 17:18:06 -0400

    clase de pug

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const routes = require('./routes')
+const productsRoutes = require('./routes/products')
 
 app.set('port', process.env.PORT || 3000)
   .set('views', path.join(__dirname,'views'))
   .set('view engine', 'pug')
 
-app.get('/products', routes.productsRoutes)
+app.use('/products', productsRoutes)
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening http://localhost:${server.address().port}`);
