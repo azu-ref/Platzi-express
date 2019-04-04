d327fc2255648fc430153fcdfed91d0fe1aba088
Fernando Azuaje <fjah17@gmail.com>
2019-04-04 17:42:02 -0400

    clase de handlebars

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const engines = require('consolidate')
 const routes = require('./routes')
+
+app.engine('hbs', engines.handlebars)
 
 app.set('port', process.env.PORT || 3000)
   .set('views', path.join(__dirname,'views'))
-  .set('view engine', 'pug')
+  .set('view engine', 'hbs')
 
-app.get('/products', routes.productsRoutes)
+app.get('/', (req, res)=>{
+  res.render("index", {hello: "hola", world: "mundo"})
+})
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening http://localhost:${server.address().port}`);
