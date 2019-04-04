e958f0be50d65dcb571905ab563384db85728283
Fernando Azuaje <fjah17@gmail.com>
2019-04-04 18:37:29 -0400

    ejemplo de clase de pug en hbs

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const engines = require('consolidate')
-const routes = require('./routes')
+const engines = require('consolidate');
+const productsRouter = require('./routes/products');
 
 app.engine('hbs', engines.handlebars)
 
 app.set('port', process.env.PORT || 3000)
   .set('views', path.join(__dirname,'views'))
-  .set('view engine', 'hbs')
+  .set('view engine', 'hbs');
 
 app.get('/', (req, res)=>{
-  res.render("index", {hello: "hola", world: "mundo"})
+  res.render("index", {hello: "hola", world: "mundo"});
 })
+
+app.use('/products', productsRouter);
+
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening http://localhost:${server.address().port}`);
