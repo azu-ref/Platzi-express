776523ffda2d3294ed9594fe45507c963b76ff87
Fernando Azuaje <fjah17@gmail.com>
2019-04-04 20:57:44 -0400

    clase de construccion de un ecomerce con pug y bulma

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const products = [
     {
         name: "Red Shoes",
-        price: 75
+        price: 75,
+        image: "http://www.adpflooring.co.uk/images/category_3/Spring%20And%20Autumn%20adidas%20Running%20Duramo%208%20Core%20RedFootwear%20WhiteCore%20Black%20BlueFootwear%20WhiteEnergy%202LB42H7KU.jpg"
     },
     {
         name: "Black Bike",
-        price: 300
+        price: 300,
+        image: "https://trek.scene7.com/is/image/TrekBicycleProducts/Marlin7_17382_A_Portrait?$responsive-pjpg$&wid=1920&hei=1440"
     }
 ];
 
