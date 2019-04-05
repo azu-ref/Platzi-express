9077a798e6c5657ed809b8b0865cdb5c9a7a1ce5
Fernando Azuaje <fjah17@gmail.com>
2019-04-04 22:49:22 -0400

    use static files .css ang images

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const app = express();
 const productsRoutes = require('./routes/products')
 
+app.use("/static", express.static(path.join(__dirname, 'public')))
+
 app.set('port', process.env.PORT || 3000)
   .set('views', path.join(__dirname,'views'))
   .set('view engine', 'pug')
